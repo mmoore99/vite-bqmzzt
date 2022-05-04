e512d2ddcae8ff2dd107a1bc7a4717ee8e08b23b
mmoore99 <mike@fiddlebits.net>
2022-05-04 11:24:18 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Hello there, {{ name }}!</div>
+  <Form v-model="name" @submit="submitForm" ref="formRef" />
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Form from './components/Form.vue';
+
+const name = ref('');
+const formRef = ref(null);
+
+function submitForm() {
+  console.log(name.value);
+  console.log('formRef', formRef.value);
+  debugger;
+}
+</script>
---
+++ src/components/Form.vue
@@ -0,0 +1,43 @@
+<template>
+  <form @submit.prevent="submitHandler">
+    <label
+      >Name
+      <input v-model="name" type="text" />
+    </label>
+    <button>Submit</button>
+  </form>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: '',
+  },
+});
+const emit = defineEmits(['update:modelValue', 'submit']);
+
+const name = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(val) {
+    emit('update:modelValue', val);
+  },
+});
+
+function submitHandler() {
+  console.log('Log from form component - submitHandler');
+  emit('submit');
+}
+
+function testLog() {
+  console.log('Log from form component');
+}
+
+defineExpose({
+  submitHandler,
+  testLog,
+});
+</script>
